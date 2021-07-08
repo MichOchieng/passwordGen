e5856b54f618ae16854b4894a5350b83098e4f57
MichOchieng <MichOchieng@gmail.com>
2021-07-08 16:44:53 -0700

    First commit

---
+++ src/components/pageHeader.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+    render(){
+        return(
+            <div className = "Header">
+                <h1>Test</h1>
+                <h2>Tester</h2>
+            </div>
+        );
+    }
+    
+}
+export default Header;
