f85c2b7702c8a1c34a63b220d49b079bcb12c427
MichOchieng <MichOchieng@gmail.com>
2021-07-08 23:53:50 -0700

    Changed styles

---
+++ src/components/passswordOptions.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import '../styles/options.css'
+
+class Options extends Component{
+    render(){
+        return(
+            <div className = "Options">
+                <h1>Test</h1>
+                
+            </div>
+        );
+    }
+    
+}
+export default Options;
