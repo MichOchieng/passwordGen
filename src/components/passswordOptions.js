67f5a63cdb796eb44b77176924577dffdb8c7512
MichOchieng <MichOchieng@gmail.com>
2021-07-09 14:14:30 -0700

    Added slider inputs

--- src/components/passswordOptions.js
+++ src/components/passswordOptions.js
@@ -5,9 +5,16 @@ class Options extends Component{
     render(){
         return(
             <div className = "Options">
-                <h1>Password Options</h1>
+                <h1 id = "optionH1">Password Options</h1>
                 
+                <div class="sliderContainer">
+                    <input id="sliderInfo"></input>
+                    <input type="range" min="6" max="256" value="6" class="slider" id="mySlider"></input>
+                    
+                </div>
+
             </div>
+            
         );
     }
     
