c8f5e5ab2bed8d166cb2c96adb9f6a4503bf8dc2
MichOchieng <MichOchieng@gmail.com>
2021-07-08 17:30:57 -0700

    Added header text

--- src/components/pageHeader.js
+++ src/components/pageHeader.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
+import '../styles/header.css'
 
 class Header extends Component{
     render(){
         return(
             <div className = "Header">
-                <h1>Test</h1>
-                <h2>Tester</h2>
+                <h1>Password Generator</h1>
+                <i>&#128273;</i>
             </div>
         );
     }
