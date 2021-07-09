39418576011c301efa221fbf1bd6208b744ff516
MichOchieng <MichOchieng@gmail.com>
2021-07-08 23:50:23 -0700

    Added containers and rows to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import './styles/App.css';
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+
 import Header from './components/pageHeader';
+import Preview from './components/passwordPreview';
+import Options from './components/passswordOptions';
+
+import './styles/App.css';
 
 function App() {
   return (
     <div className="App-body">
-        <Header />
+      <Container fluid>
+        <Row>
+          <Header/>
+        </Row>
+
+        <Row>
+          <Preview/>
+        </Row>
+
+        <Row>
+          <Options/>
+        </Row>
+      </Container>
     </div>
   );
 }
