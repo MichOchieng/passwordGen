681aea35d26ab6504fcd2c289c671e263409e163
MichOchieng <MichOchieng@gmail.com>
2021-07-09 13:01:46 -0700

    Removed button borders

--- src/components/passwordPreview.js
+++ src/components/passwordPreview.js
@@ -10,10 +10,10 @@ class Preview extends Component{
             <div className = "Preview">
                 <InputGroup>
                     <FormControl id= "noBorder" placeholder = "Password"></FormControl>
-                    <Button variant="outline-secondary" id="button-addon2 noBorder" data-toggle="tooltip" data-placement="right" title="Copy">
+                    <Button variant="outline-secondary" class="btn btn-link" id="noBorder" data-toggle="tooltip" data-placement="right" title="Copy">
                         <FontAwesomeIcon icon={faCopy}/>    
                     </Button>
-                    <Button variant="outline-secondary" id="button-addon2 noBorder" data-toggle="tooltip" data-placement="right" title="Refresh">
+                    <Button variant="outline-secondary" class="btn btn-link" id="noBorder" data-toggle="tooltip" data-placement="right" title="Refresh">
                         <FontAwesomeIcon icon={faSync}/>
                     </Button>
                 </InputGroup>
