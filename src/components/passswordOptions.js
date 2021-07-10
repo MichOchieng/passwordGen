b9691e75508d3cf8c979703729f86c646f56acab
MichOchieng <MichOchieng@gmail.com>
2021-07-09 18:36:29 -0700

    Added functioning slider and slider value

--- src/components/passswordOptions.js
+++ src/components/passswordOptions.js
@@ -1,26 +1,50 @@
-import React, {Component} from 'react';
-import '../styles/options.css'
+import React, {Component, useState} from 'react';
+import '../styles/options.css';
 
-// Slider logic
-const slider = document.getElementById("mySlider");
-const info = document.getElementById("sliderInfo");
-info.innerHTML = slider.value;
-slider.oninput = function() {
-    info.innerHTML = this.value;
-}
 
 class Options extends Component{
+    constructor(props){
+        super(props);
+        this.state = { // Used for initial slider value 
+            count: 6
+        };
+    }
+
+    // Allows slider value to be updated visually by simulating a click on the input box
+    updateSlider(){ 
+        document.getElementById("sliderInfo").click();
+    }
+    // Updates the value of the input box
+    view(){
+        document.getElementById("sliderInfo").value = document.getElementById("mySlider").value;
+    }
+    
     render(){
         return(
             <div className = "Options">
                 <h1 id = "optionH1">Password Options</h1>
-                
+                {/* Inputs for slider */}
                 <div class="sliderContainer">
-                    <input id="sliderInfo" type="text" readonly></input>
-                    <input type="range" min="6" max="256" class="slider" id="mySlider"></input>
-                    
+                    <input 
+                        id="sliderInfo" 
+                        type="text"
+                        value={this.state.count}
+                        onClick={this.view}
+                        readOnly></input>
+                    <input 
+                        type="range" 
+                        min={this.state.count} 
+                        max="256" 
+                        defaultValue="6"
+                        class="slider" 
+                        id="mySlider"
+                        onInput={this.updateSlider}
+                        ></input>
+                      
                 </div>
-                
+                {/* Buttons for other options */}
+
+
             </div>
             
         );
