5ee1387ad7337867e76bbf5a74dcf306b908fb56
MichOchieng <MichOchieng@gmail.com>
2021-07-11 16:13:57 -0700

    Removed generatePassword function and added to scripts folder for communal use

--- src/components/passwordPreview.js
+++ src/components/passwordPreview.js
@@ -3,6 +3,7 @@ import { faSync,faCopy } from '@fortawesome/free-solid-svg-icons'
 import React, {Component} from 'react';
 import { Button, FormControl, InputGroup } from 'react-bootstrap';
 import '../styles/preview.css'
+import generartePassword from '../scripts/passGen'
 
 class Preview extends Component{
     constructor(props){
@@ -10,57 +11,13 @@ class Preview extends Component{
         this.state = {
             password: ""
         }; 
-        this.generartePassword = this.generartePassword.bind(this);
+        // this.generartePassword = this.generartePassword.bind(this);
         this.copyPassword      = this.copyPassword.bind(this);
     }
 
     // Will load the initial password to the preview box 
     componentDidMount(){
-        this.generartePassword();
-    }
-    
-    generartePassword(){
-        // Clear password
-        this.state.password = "";
-        // Alphabets
-        const upperAlphabet  = "QWERTYUIOPASDFGHJKLZXCVBNM";
-        const lowerAlphabet  = "qwertyuiopasdfghjklzxcvbnm";
-        const symbolAlphabet = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
-        const numbers        = "1234567890";
-        
-        // Get selected options 
-        var passLength   = document.getElementById("sliderInfo").value;
-        var hasUppercase = document.getElementById("uppercaseButton").value;
-        var hasLowercase = document.getElementById("lowercaseButton").value;
-        var hasNums      = document.getElementById("numberButton").value;
-        var hasSymbols   = document.getElementById("symbolsButton").value;
-
-        // Check password critria
-        for (let index = 0; index <= passLength; index++) {
-            if (hasUppercase && index<=passLength) {
-                // Add uppercase char to password
-                this.state.password += upperAlphabet[Math.floor(Math.random()*upperAlphabet.length)];
-                index++;
-            }  
-            if (hasLowercase && index<=passLength) {
-                // Add lowercase char to password
-                this.state.password += lowerAlphabet[Math.floor(Math.random()*lowerAlphabet.length)];
-                index++;
-            }
-            if (hasNums && index<=passLength) {
-                // Add number to password
-                this.state.password += numbers[Math.floor(Math.random()*numbers.length)];
-                index++;
-            }
-            if (hasSymbols && index<=passLength) {
-                // Add symbol char to password
-                this.state.password += symbolAlphabet[Math.floor(Math.random()*symbolAlphabet.length)];
-                index++;
-            }
-            
-        }
-        // Return password 
-        document.getElementById("passwordPreview" ).value = this.state.password
+        generartePassword(this.state.password);
     }
 
     copyPassword(){
@@ -102,7 +59,7 @@ class Preview extends Component{
                         title="Refresh"
                         onClick={
                             () => {
-                                this.generartePassword()
+                                generartePassword()
                             }
                         }
                         >
