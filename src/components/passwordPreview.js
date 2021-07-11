4a170ece21befc47bd7d8dd1ebff616852f76b7f
MichOchieng <MichOchieng@gmail.com>
2021-07-10 19:51:34 -0700

    Added methods for password generation

--- src/components/passwordPreview.js
+++ src/components/passwordPreview.js
@@ -5,6 +5,34 @@ import { Button, FormControl, InputGroup } from 'react-bootstrap';
 import '../styles/preview.css'
 
 class Preview extends Component{
+    // constructor(props){
+    //     super(props);
+    //     this.
+    // }
+
+    // Will load the initial password to the preview box 
+    componentDidMount(){
+        
+    }
+
+    generartePassword(password){
+        // Alphabets
+        const upperAlphabet  = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        const lowerAlphabet  = "qwertyuiopasdfghjklzxcvbnm";
+        const symbolAlphabet = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
+        const numbers        = "1234567890";
+        
+        // Get selected options 
+        var passLength   = document.getElementById("sliderInfo").value;
+        var hasUppercase = document.getElementById("uppercaseButton").value;
+        var hasLowercase = document.getElementById("lowercaseButton").value;
+        var hasNums      = document.getElementById("numberButton").value;
+        var hasSymbols   = document.getElementById("symbolsButton").value;
+
+        
+        // 
+    }
+
     render(){
         return(
             <div className = "Preview">
